e85591b3ba7d97e601c69bd589ec2ade9ac20318
shinriyo <shinriyo@gmail.com>
2024-08-11 17:39:35 +0900

    main.dart fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -32,7 +32,7 @@ class MyHomePage extends StatelessWidget {
   }
 
   void _pickNumber(BuildContext context) async {
-    final result = await showNumpadPicker(context, length: 4, isFloat: true);
+    final result = await showNumpadPicker(context, length: 8, isFloat: true);
     if (result != null) {
       if (context.mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
